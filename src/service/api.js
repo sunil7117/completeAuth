f2daadc4b95987f5e9279e4771bdf26bb409e0a9
sunil7117 <sunilapp7117@gmail.com>
2023-10-13 06:59:57 +0530

    Now register a user api is ready and working with frontend.

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,14 @@
+import axios from 'axios'
 const api="http://localhost:8000"
 
-export const Signup=async()=>{}
+export const Signup=async(data)=>{
+     try {
+        return await axios.post(`${api}/api/auth/signup`,data)
+     } catch (error) {
+        console.error(error.response)
+        return(error.response)
+     }
+}
 export const Login=async()=>{}
 export const ForgetPassword=async()=>{}
 export const OTPVerify=async()=>{}
