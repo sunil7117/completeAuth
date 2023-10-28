939691788ddf11a1a55c0277835b226c59cc94eb
sunil7117 <sunilapp7117@gmail.com>
2023-10-28 15:59:54 +0530

    contact initialed

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import OTPVerify from './components/OTPVerify';
 import Home from './pages/Home';
 import UpdatePassword from './components/UpdatePassword';
 import ProtectedRoute from './ProtectedRoute';
+import Contact from './pages/Contact';
 // import Shop from './pages/Shop';
-// import Contact from './pages/Contact';
 // import Blog from './pages/Blog';
 // import Error from './pages/Error';
 function App() {
@@ -22,8 +22,8 @@ function App() {
       <Route path="/forgetpassword" element={<Forget/>} />
       <Route path="/otpverification" element={<OTPVerify/>} />
       <Route path="/update-password" element={<UpdatePassword/>} />
-      {/* <Route path='/shop' element={<Shop/>}/>
       <Route path='/contact' element={<Contact/>}/>
+      {/* <Route path='/shop' element={<Shop/>}/>
       <Route path='/blog' element={<Blog/>}/>
       <Route path='*' element={<Error/>}/> */}
     </Routes>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Contact = () => {
+  return (
+    <>
+      <Header />
+      <h1>Contact</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Contact;
