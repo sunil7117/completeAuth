df5456cfebf0497a8b5d7820ac0115b94f2c6909
sunil7117 <sunilapp7117@gmail.com>
2023-10-28 15:53:08 +0530

    shop initialed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import OTPVerify from './components/OTPVerify';
 import Home from './pages/Home';
 import UpdatePassword from './components/UpdatePassword';
 import ProtectedRoute from './ProtectedRoute';
-// import Shop from './pages/Shop';
+import Shop from './pages/Shop';
 // import Contact from './pages/Contact';
 // import Blog from './pages/Blog';
 // import Error from './pages/Error';
@@ -22,8 +22,8 @@ function App() {
       <Route path="/forgetpassword" element={<Forget/>} />
       <Route path="/otpverification" element={<OTPVerify/>} />
       <Route path="/update-password" element={<UpdatePassword/>} />
-      {/* <Route path='/shop' element={<Shop/>}/>
-      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/shop' element={<Shop/>}/>
+      {/* <Route path='/contact' element={<Contact/>}/>
       <Route path='/blog' element={<Blog/>}/>
       <Route path='*' element={<Error/>}/> */}
     </Routes>
---
+++ src/pages/Shop.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const Shop = () => {
+  return (
+    <>
+      <Header />
+      <h1>shop page</h1>
+      <Footer />
+    </>
+  );
+};
+
+export default Shop;
