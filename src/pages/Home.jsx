fca1c8cd7225dab0d1ca6b607bbc19f729c3dc91
sunil7117 <sunilapp7117@gmail.com>
2023-10-28 15:16:13 +0530

    misiliniace file added here some confusion to adding in stage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./home.css";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 const Home = () => {
   return (
     <>
       <Header />
       <h1>Home</h1>
+      <Footer />
     </>
   );
 };
